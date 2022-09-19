cd01f3f266ff933bf37e20a63223cc7be29387ad
JunBeezy <smobsoftware@gmail.com>
2022-09-19 04:40:49 -0500

    update categories

--- pages/api/products/categories.js
+++ pages/api/products/categories.js
@@ -3,7 +3,7 @@ import nc from 'next-connect';
 const handler = nc();
 
 handler.get(async (req, res) => {
-  const categories = ['Shirts', 'Pants'];
+  const categories = ['Shirts', 'Pants', 'Accessories', 'Shoes', 'Jackets'];
   res.send(categories);
 });
 
